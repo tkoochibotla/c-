0702b420341992cc044292cb4fce71c85afaa21c
tkoochibotla <tejkoochibotla@gmail.com>
2021-01-24 20:09:59 +0530

    Update TestAutomation.groovy

--- TestAutomation.groovy
+++ TestAutomation.groovy
@@ -1,16 +1,9 @@
 node {
-    stage("scm") {
-        git branch: 'master', url: 'https://github.com/tkoochibotla/c-.git'    }
-    stage("build") {
-        dir(basePath) {
-            bat "${nuggetPath} restore TestAutomation.sln"
-            bat "MSBuild.exe TestAutomation.sln /t:Clean;Build /p:Configuration=Dev_Env" 
-        }
-    }
-    stage("run tests") {
-        def executeTestCmd = "${nunitPath} Features/bin/${buildConfig}/Features.dll"
-        dir(basePath) {
-            bat(script: "${executeTestCmd}", returnStatus: true)
-        }
-    }
-    }
+  stage('HelloWorld') {
+    echo 'Hello World'
+  }
+
+  stage('git clone') {
+    git clone "https://github.com/tkoochibotla/c-.git"
+  }
+}
