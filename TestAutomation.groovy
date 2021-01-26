d03b306b27b5d9f5f0ebec955c4705daae4ba1c8
tkoochibotla <tejkoochibotla@gmail.com>
2021-01-26 12:22:40 +0530

    Update TestAutomation.groovy

--- TestAutomation.groovy
+++ TestAutomation.groovy
@@ -3,5 +3,10 @@ node {
         // Get some code from a GitHub repository
         git branch: 'master', url: 'https://github.com/tkoochibotla/c-.git'
     }
+   stage('Test') { // for display purposes
+        // Get some code from a GitHub repository
+       bat 'dotnet test'
+    }
+   
      }
  
